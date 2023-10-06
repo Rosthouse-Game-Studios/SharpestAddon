85f69b927e134b5223f9f77436ed2ef6f8c5e06c
rosthouse <rosthouse@gmail.com>
2023-10-06 19:43:59 +0200

    Adding rectextensions

--- Extensions/RectExtensions.cs
+++ Extensions/RectExtensions.cs
@@ -5,14 +5,14 @@ using Godot;
 
 namespace rosthouse.sharpest.addons
 {
-	public static class RectExtensions
-	{
-		public static Vector2 RandomPointInside(this Rect2 rect)
-		{
-			return new Vector2(
-				(float)GD.RandRange(rect.Position.X, rect.End.X),
-				(float)GD.RandRange(rect.Position.Y, rect.End.Y)
-			);
-		}
-	}
+  public static class RectExtensions
+  {
+    public static Vector2 RandomPointInside(this Rect2 rect)
+    {
+      return new Vector2(
+        (float)GD.RandRange(rect.Position.X, rect.End.X),
+        (float)GD.RandRange(rect.Position.Y, rect.End.Y)
+      );
+    }
+  }
 }
