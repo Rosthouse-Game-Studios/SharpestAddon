f54767a450922345f8783b22d393b0dd3c670dc8
rosthouse <rosthouse@gmail.com>
2025-01-05 21:29:11 +0100

    Fixing some issues

--- addons/SharpestAddon/Extensions/GodotObjectExtensions.cs
+++ addons/SharpestAddon/Extensions/GodotObjectExtensions.cs
@@ -6,21 +6,28 @@ public static class GodotObjectExtensions
 {
 
   public static bool TryGetMeta(this GodotObject r, StringName name, out Variant value)
-  { 
-    if(r.HasMeta(name)){
+  {
+    if (r.HasMeta(name))
+    {
       value = r.GetMeta(name);
       return true;
     }
     value = default;
     return false;
   }
+
   public static bool TryGetMeta<[MustBeVariant] T>(this GodotObject r, StringName name, out T value)
-  { 
-    if(r.HasMeta(name)){
+  {
+    if (r.HasMeta(name))
+    {
       value = r.GetMeta(name).As<T>();
       return true;
     }
+
+#pragma warning disable CS8601 // Possible null reference assignment.
     value = default;
+#pragma warning restore CS8601 // Possible null reference assignment.
+
     return false;
   }
 }
--- addons/SharpestAddon/Extensions/RayCastExtensions.cs
+++ addons/SharpestAddon/Extensions/RayCastExtensions.cs
@@ -7,7 +7,9 @@ public static class RayCastExtensions
 
   public static bool TryIsCollding(this RayCast3D r, out Node3D collider)
   {
+#pragma warning disable CS8601 // Possible null reference assignment.
     collider = r.GetCollider() as Node3D;
+#pragma warning restore CS8601 // Possible null reference assignment.
     return collider != null;
   }
 }
