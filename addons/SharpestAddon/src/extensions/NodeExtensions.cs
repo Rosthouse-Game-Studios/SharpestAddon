97949aa0b373a46c6ca9bff91c6bd4e6f6c5af0a
Rosthouse <rosthouse@gmail.com>
2025-01-27 11:25:03 +0100

    Adding new documentation to node extensions

--- addons/SharpestAddon/src/extensions/NodeExtensions.cs
+++ addons/SharpestAddon/src/extensions/NodeExtensions.cs
@@ -7,6 +7,16 @@ namespace rosthouse.sharpest.addon;
 public static class NodeExtensions
 {
 
+  /// <summary>
+  /// Get's all children of a <see cref="Node"/> fitting a specific type. They have to be a <see cref="Variant"/>.
+  ///
+  /// This method does not guarantee any ordering of the nodes.
+  /// </summary>
+  /// <typeparam name="T">The type we whish the children to be. Must be a <see cref="Variant"/>.</typeparam>
+  /// <param name="n">The node for which to search children</param>
+  /// <param name="includeInternal">If nodes not visible in the scene tree should be considered.</param>
+  /// <param name="recursive">If the scene tree should be searched recursively.</param>
+  /// <returns>A <see cref="Godot.Collections.Array{T}"/>, with all children fitting <see cref="{T}"/>. Will always return at least an emtpy array.</returns>
   public static Godot.Collections.Array<T> GetChildren<[MustBeVariant] T>(this Node n, bool includeInternal = false, bool recursive = false) where T : Node
   {
     var arr = new Godot.Collections.Array<T>();
@@ -15,11 +25,21 @@ public static class NodeExtensions
       if (c is T t)
       {
         arr.Add(t);
+        if(recursive){
+          arr.AddRange(t.GetChildren<T>(includeInternal, recursive));
+        }
       }
     }
     return arr;
   }
 
+  /// <summary>
+  /// Checks if the current <see cref="Node"/> has any <see cref="{T}"/> as a child.
+  /// </summary>
+  /// <typeparam name="T">Must be a <see cref="Variant"/></typeparam>
+  /// <param name="n">The node tha</param>
+  /// <param name="includeInternal">If internal children should also be searched</param>
+  /// <returns>true, if any fitting node was found, false otherwise.</returns>
   public static bool HasChild<[MustBeVariant] T>(this Node n, bool includeInternal = false) where T : Node
   {
     foreach (var c in n.GetChildren(includeInternal))
@@ -32,7 +52,15 @@ public static class NodeExtensions
     return false;
   }
 
-  public static T? GetChildByType<[MustBeVariant] T>(this Node n, bool includeInternal = false) where T : Node
+  /// <summary>
+  /// Gets the first child of a <see cref="Node"/> fitting the type parameter.
+  /// </summary>
+  /// <typeparam name="T">A type of <see cref="Variant"/></typeparam>
+  /// <param name="n">The node for which we want to get a child.</param>
+  /// <param name="includeInternal">If internal nodes should also be considered.</param>
+  /// <param name="recursive">If the tree should be searched recursively. This enables depth-first search</param>
+  /// <returns>The found node, null otherwise</returns>
+  public static T? GetFirstChildByType<[MustBeVariant] T>(this Node n, bool includeInternal = false, bool recursive = false) where T : Node
   {
     foreach (var c in n.GetChildren(includeInternal))
     {
@@ -40,52 +68,13 @@ public static class NodeExtensions
       {
         return (T)c;
       }
-    }
-    return null;
-  }
-
-  public static Godot.Collections.Array<Node> GetChildrenRecursive(this Node n, bool includeInternal = false)
-  {
-    var children = new Godot.Collections.Array<Node>();
-    foreach (var c in n.GetChildren(includeInternal))
-    {
-      children.Add(c);
-      children.AddRange(c.GetChildrenRecursive(includeInternal));
-    }
-    return children;
-  }
-
-  public static T? GetChildRecursive<[MustBeVariant] T>(this Node n, bool includeInternal = false) where T : Node
-  {
-    var children = new Godot.Collections.Array<Node>();
-    foreach (var c in n.GetChildren(includeInternal))
-    {
-      if (c is T)
-      {
-        return (T)c;
-      }
-      var t = c.GetChildRecursive<T>(includeInternal);
+      var t = c.GetFirstChildByType<T>(includeInternal);
       if (t != null)
       {
         return t;
       }
     }
     return null;
-  }
-
-  public static Godot.Collections.Array<T> GetChildrenRecursive<[MustBeVariant] T>(this Node n, bool includeInternal = false) where T : Node
-  {
-    var children = new Godot.Collections.Array<T>();
-    foreach (var c in n.GetChildren(includeInternal))
-    {
-      if (c is T)
-      {
-        children.Add((T)c);
-      }
-      var t = c.GetChildrenRecursive<T>(includeInternal);
-      children.AddRange(t);
-    }
-    return children;
   }
 
   /// <summary>
@@ -106,7 +95,13 @@ public static class NodeExtensions
     return null;
   }
 
-  public static void QueueFreeChildren(this Node n, bool includeInternal = false){
+  /// <summary>
+  /// Calls <see cref="Node.QueueFree"/> on all children of the calling node.
+  /// </summary>
+  /// <param name="n">Free all children of this node</param>
+  /// <param name="includeInternal">If the internal children (not visible in the scene tree) should also be freed.</param>
+  public static void QueueFreeChildren(this Node n, bool includeInternal = false)
+  {
     foreach (var c in n.GetChildren(includeInternal))
     {
       c.QueueFree();
