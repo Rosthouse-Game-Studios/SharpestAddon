e49fef5f0d4e10a7b30cc1055d16241bcce22aa4
Rosthouse <rosthouse@gmail.com>
2026-09-05 19:54:16 +0200

    Renaming DegToRadEditor to DegToRadEditorProperty (#6)
    Removing imports, as these have to be done by each project separately

--- src/nodes/FiniteStateMachine/StateMachine.cs
+++ src/nodes/FiniteStateMachine/StateMachine.cs
@@ -1,58 +1,54 @@
-using System;
-using System.Diagnostics;
-using System.Threading.Tasks;
 using Godot;
-using Godot.Collections;
 using rosthouse.sharpest.addon.utils;
 
 
 namespace rosthouse.sharpest.addon.nodes.finitestatemachine;
 
 [GlobalClass]
-public partial class StateMachine: Node
+public partial class StateMachine : Node
 {
-    [Export]
-    public State InitialState { get; private set; } = null!;
-    public State CurrentState { get; private set; } = null!;
+  [Export]
+  public State InitialState { get; private set; } = null!;
+  public State CurrentState { get; private set; } = null!;
 
-    public override async void _Ready()
+  public override async void _Ready()
+  {
+    base._Ready();
+
+    foreach (var state in this.GetChildren<State>())
     {
-        base._Ready();
-
-        foreach(var state in this.GetChildren<State>())
-        {
-            state.Finished += TransitionNext;
-        }
-
-        await ToSignal(Owner, Node.SignalName.Ready);
-        CurrentState = InitialState;
-        CurrentState.Enter();
+      state.Finished += TransitionNext;
     }
 
-    private void TransitionNext(NodePath nextStatePath)
-    {
-        var nextState = GetNode<State>(nextStatePath);
-        DebugUtils.PrintDebug($"[{Owner.Name}] Switching from state {CurrentState.Name} to {nextState.Name}");
-        CurrentState.Exit();
-        nextState.Enter(CurrentState.GetPath());
-        CurrentState=nextState;
-    }
+    await ToSignal(Owner, Node.SignalName.Ready);
+    CurrentState = InitialState;
+    CurrentState.Enter();
+  }
 
-    public override void _UnhandledInput(InputEvent @event)
-    {
-        base._UnhandledInput(@event);
-        CurrentState.HandleInput(@event);
-    }
+  private void TransitionNext(NodePath nextStatePath)
+  {
+    var nextState = GetNode<State>(nextStatePath);
+    DebugUtils.PrintDebug($"[{Owner.Name}] Switching from state {CurrentState.Name} to {nextState.Name}");
+    CurrentState.Exit();
+    nextState.Enter(CurrentState.GetPath());
+    CurrentState = nextState;
+  }
 
-    public override void _Process(double delta)
-    {
-        base._Process(delta);
-        CurrentState.Update(delta);
-    }
+  public override void _UnhandledInput(InputEvent @event)
+  {
+    base._UnhandledInput(@event);
+    CurrentState.HandleInput(@event);
+  }
 
-    public override void _PhysicsProcess(double delta)
-    {
-        base._PhysicsProcess(delta);
-        CurrentState.PhysicsUpdate(delta);
-    }
+  public override void _Process(double delta)
+  {
+    base._Process(delta);
+    CurrentState.Update(delta);
+  }
+
+  public override void _PhysicsProcess(double delta)
+  {
+    base._PhysicsProcess(delta);
+    CurrentState.PhysicsUpdate(delta);
+  }
 }
